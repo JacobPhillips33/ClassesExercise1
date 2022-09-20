cf70c53e1ac2284ea935879b2651c95bbce22267
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-19 22:13:29 -0500

    initial commit

---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassesExercise1;
+
+var elantra = new Car();
+elantra.Make = "Hyundai";
+elantra.Model = "Elantra";
+elantra.Year = 2015;
+
+Console.WriteLine($"Make: {elantra.Make}");
+Console.WriteLine($"Model: {elantra.Model}");
+Console.WriteLine($"Year: {elantra.Year}");
